e41e7d2190a33fb8d8b0af11da2d79c1a6ae80f8
Carlos Sosa <carlossosa@SedexITs-MacBook-Pro.local>
2022-06-21 13:03:12 +0100

    Add styled components and other configurations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GlobalStyle } from 'styles'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return <>
+		<GlobalStyle />
+		<main>
+			<Component {...pageProps} />
+		</main>
+	</>
 }
 
-export default MyApp
+export default App
