990e7c2e0a9b7585c11e7f0c77114652d37f702e
Carlos Sosa <cesosag@gmail.com>
2023-05-27 15:19:21 +0100

    Upgrade packages and adjust docker files

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -117,8 +117,8 @@ export const getStaticProps: GetStaticProps<HomeProps> = async () => {
 			},
 			podcasts: {
 				title: 'Escucha nuestro podcast',
-				spotifyURL: 'https://open.spotify.com/show/3BQmoqVfbP3PzhQVSmkCBV?si=1a82e165ac604aea',
-				applePodcastsURL: 'https://podcasts.apple.com/podcast/id1669415564?app=podcast&at=1000lHKX',
+				spotifyURL: 'https://open.spotify.com/show/3BQmoqVfbP3PzhQVSmkCBV',
+				applePodcastsURL: 'https://podcasts.apple.com/podcast/id1669415564',
 				googlePodcastsURL: 'https://podcasts.google.com/?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy9kYTM0ZDQ0NC9wb2RjYXN0L3Jzcw',
 				castboxURL: 'https://castbox.fm/vic/1669415564'
 			},
