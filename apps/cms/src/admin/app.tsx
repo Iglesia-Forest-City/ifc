58dddb7accd4d7b8f7614121cb9e72eda6cf2582
Carlos Sosa <cesosag@gmail.com>
2023-06-29 16:50:03 +0100

    Add custom logo to config

---
+++ apps/cms/src/admin/app.tsx
@@ -0,0 +1,14 @@
+import Logo from './extensions/fc_logo.svg'
+
+export default {
+	config: {
+		locales: ['es', 'en'],
+		auth: {
+			logo: Logo
+		},
+		menu: {
+			logo: Logo
+		}
+  },
+  bootstrap() {},
+};
