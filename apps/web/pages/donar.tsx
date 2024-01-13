601270f1f50971fe82425acef5a50a0cc22720be
Carlos Sosa <cesosag@gmail.com>
2024-01-13 17:57:33 +0000

    Fix donations redirection on IP detection

--- apps/web/pages/donar.tsx
+++ apps/web/pages/donar.tsx
@@ -2,33 +2,34 @@ import { useEffect } from 'react'
 import type { GetServerSideProps } from 'next'
 import type { IPResponse } from 'services'
 import { ip } from 'services'
-import { getIpFromHeader } from 'utils'
 
 export type DonateProps = {
 	donationsURL: string
 }
 
-const Donate = ({ donationsURL }: DonateProps) => {
+const Donate = () => {
 	useEffect(() => {
-		window.location.href = donationsURL
-	}, [donationsURL])
+		let donationsURL = 'https://adventistgiving.org/#/org/ANTBEV/envelope/start'
+		const donationsURLInternational = 'https://www.paypal.com/donate/?hosted_button_id=4SR42CJAZJLNN'
+		const getIp = async () => {
+			try {
+				const { data } = await ip.get<IPResponse>(`/json/`);
+				donationsURL = data.country_code === 'US' ? donationsURL : donationsURLInternational
+			} catch (err) {
+				console.error(err)
+				throw err
+			} finally {
+				window.location.href = donationsURL
+			}
+		}
+		getIp();
+	}, [])
 	return null
 }
 
 export default Donate
 
-export const getServerSideProps: GetServerSideProps<DonateProps> = async ({ req }) => {
-	let donationsURL = 'https://adventistgiving.org/#/org/ANTBEV/envelope/start'
-	const donationsURLInternational = 'https://www.paypal.com/donate/?hosted_button_id=4SR42CJAZJLNN'
-	try {
-		const headerIp = Array.isArray(req.headers['x-forwarded-for']) ? req.headers['x-forwarded-for'][0] : req.headers['x-forwarded-for']
-		const clientIP = headerIp && getIpFromHeader(headerIp) || req.socket.remoteAddress;
-		const { data } = await ip.get<IPResponse>(`${clientIP}/json/`)
-		donationsURL = data.country_code === 'US' ? donationsURL : donationsURLInternational
-	} catch (err) {
-		console.error(err)
-		throw err
-	}
+export const getServerSideProps: GetServerSideProps = async () => {
 	return {
 		props: {
 			header: {
@@ -41,8 +42,7 @@ export const getServerSideProps: GetServerSideProps<DonateProps> = async ({ req 
 				logo: '/fcradio-logo.png',
 				logoAltText: 'Forest City Radio',
 				url: '/live-radio'
-			},
-			donationsURL,
+			}
 		},
 	}
 }
